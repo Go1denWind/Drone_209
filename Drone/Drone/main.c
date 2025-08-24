da4646f8c337a8fef03adccbbbcd96701ab17669
Go1denWind <msun711@aucklanduni.ac.nz>
2025-08-24 23:39:46 +1200

    Create C file for drone

---
+++ Drone/Drone/main.c
@@ -0,0 +1,18 @@
+/*
+ * Drone.c
+ *
+ * Created: 24/08/2025 10:41:00 pm
+ * Author : MingzeSun
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
